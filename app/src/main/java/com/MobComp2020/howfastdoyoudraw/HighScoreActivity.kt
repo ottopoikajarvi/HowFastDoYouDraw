93d37bdecd25806a7dc29fa0bebb249037a651b0
Otto Poikajärvi <tootsi666@gmail.com>
2020-03-16 17:15:23 +0200

    Initial commit

---
+++ app/src/main/java/com/MobComp2020/howfastdoyoudraw/HighScoreActivity.kt
@@ -0,0 +1,12 @@
+package com.MobComp2020.howfastdoyoudraw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HighScoreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_high_score)
+    }
+}
---
+++ app/src/main/java/com/MobComp2020/howfastdoyoudraw/PlayActivity.kt
@@ -0,0 +1,12 @@
+package com.MobComp2020.howfastdoyoudraw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+    }
+}
