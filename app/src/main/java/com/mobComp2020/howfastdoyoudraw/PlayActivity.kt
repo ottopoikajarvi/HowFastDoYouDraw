2935d0566d5b10e9f084a44f85b2106344af0c66
juliushekkala <julius.hekkala@gmail.com>
2020-03-17 17:52:10 +0200

    Button OnClickListeners

--- app/src/main/java/com/mobComp2020/howfastdoyoudraw/PlayActivity.kt
+++ app/src/main/java/com/mobComp2020/howfastdoyoudraw/PlayActivity.kt
@@ -2,11 +2,28 @@ package com.mobComp2020.howfastdoyoudraw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_play.*
 
 class PlayActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play)
+
+        //Start the game
+        start_button.setOnClickListener {
+            start_button.visibility = View.GONE
+        }
+
+        //Submit high scores
+        submit_results_button.setOnClickListener {
+            //TODO
+        }
+
+        //Skip current figure
+        skip_button.setOnClickListener {
+            //TODO
+        }
     }
 }
