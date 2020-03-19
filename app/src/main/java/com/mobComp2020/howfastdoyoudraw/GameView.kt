83aef7f90b9655fdd621677b273824633fa7980e
juliushekkala <julius.hekkala@gmail.com>
2020-03-19 16:32:17 +0200

    New custom game view for the game

---
+++ app/src/main/java/com/mobComp2020/howfastdoyoudraw/GameView.kt
@@ -0,0 +1,12 @@
+package com.mobComp2020.howfastdoyoudraw
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * This is the custom game view
+ */
+class GameView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+//
+}
