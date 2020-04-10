5386e8c90137a3bf4e97eac4bc357f8a70e0e66b
Otto Poikajärvi <tootsi666@gmail.com>
2020-04-10 16:41:51 +0300

    Ver 2.0 of shape generator

--- app/src/main/java/com/mobComp2020/howfastdoyoudraw/GameView.kt
+++ app/src/main/java/com/mobComp2020/howfastdoyoudraw/GameView.kt
@@ -32,21 +32,28 @@ class GameView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     }
 
     override fun onDraw(canvas: Canvas) {
-        //
+        //Generates a path that is then drawn to canvas
 
         path.reset()
-        var xPoint = (Random.nextFloat()*0.9f + 0.09f)*canvas.width
-        var yPoint = (Random.nextFloat()*0.9f + 0.09f)*canvas.height
+        var xPoint = (Random.nextFloat()*0.85f + 0.13f)*canvas.width
+        var yPoint = (Random.nextFloat()*0.85f + 0.13f)*canvas.height
         var linesDrawn = 0
         path.moveTo(xPoint, yPoint)
-        for (i in 1..5){
+        //4 draws per path
+        for (i in 1..4){
             var lastXPoint = xPoint
             var lastYPoint = yPoint
             var limit = 0
             //Log.d("size", canvas.width.toString() + canvas.height.toString())
+            xPoint = Random.nextInt(Math.max(((lastXPoint - canvas.width) / 2).toInt(), 20),
+                Math.min(((lastXPoint + canvas.width) / 2).toInt(), canvas.width - 20)).toFloat()
+            yPoint = Random.nextInt(Math.max(((lastYPoint - canvas.height) / 2).toInt(), 20),
+                Math.min(((lastYPoint + canvas.height) / 2).toInt(), canvas.height - 20)).toFloat()
+
+            /* Original variant of shape generation, removed becuz bad
             next@ while(true) {
-                xPoint += Random.nextInt(-canvas.width / 2, canvas.width / 2)  //TODO: Korjaa tämä vähemmän vammaseksi, ilman looppia
-                yPoint += Random.nextInt(-canvas.height / 2, canvas.height / 2) //randint(max((lastYPoint-height/2), 0), min((lastYPoint+height/2), height))
+                xPoint += Random.nextInt(-canvas.width / 2, canvas.width / 2)
+                yPoint += Random.nextInt(-canvas.height / 2, canvas.height / 2)
                 if (xPoint > 10 && xPoint < canvas.width - 10 && yPoint > 10 && yPoint < canvas.height - 10) {
                     if ((xPoint - lastXPoint).absoluteValue > (canvas.width / 8) && (yPoint - lastYPoint).absoluteValue > (canvas.height / 8)) {
                         break@next
@@ -56,7 +63,7 @@ class GameView(context: Context, attrs: AttributeSet) : View(context, attrs) {
                 yPoint = lastYPoint
                 limit += 1
                 if (limit > 3 && linesDrawn > 2) break@next
-            }
+            }*/
             path.quadTo(xPoint, lastYPoint, xPoint, yPoint)
             linesDrawn += 1
 
