6cd2ce5242466ef1babcbbb457bf5307ca50ea7b
juliushekkala <julius.hekkala@gmail.com>
2020-04-20 15:29:38 +0300

    finger tracking

--- app/src/main/java/com/mobComp2020/howfastdoyoudraw/GameView.kt
+++ app/src/main/java/com/mobComp2020/howfastdoyoudraw/GameView.kt
@@ -14,11 +14,15 @@ import org.jetbrains.anko.toast
 class GameView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private var paint = Paint()
+    private var circlePaint = Paint()
     private var fingerPath = Path()
     private var drawPath = Path()
+    private var movement = Path()
+    private var movementMeas = PathMeasure()
     private var pathMeas = PathMeasure()
     private var currentCircle = Path()
     private var distance = 0f
+    private var victory = false
 
     //initialize the paint and everything else needed
     init {
@@ -28,15 +32,20 @@ class GameView(context: Context, attrs: AttributeSet) : View(context, attrs) {
         drawPath.moveTo(5.0f, 5.9f)
         drawPath.lineTo(500.0f, 400.0f)
         pathMeas.setPath(drawPath, false)
-        currentCircle.addCircle(5.0f, 5.9f, 10.0f, Path.Direction.CW)
+        currentCircle.addCircle(5.0f, 5.9f, 50.0f, Path.Direction.CW)
+
+        circlePaint.color = Color.RED
+        circlePaint.strokeWidth = 20.0f
+        circlePaint.style = Paint.Style.STROKE
+
     }
 
     override fun onDraw(canvas: Canvas) {
         //
         canvas.apply {
             drawPath(drawPath, paint)
-            if (!fingerPath.isEmpty) {
-                drawPath(fingerPath, paint)
+            if (!currentCircle.isEmpty) {
+                drawPath(currentCircle, circlePaint)
             }
 
         }
@@ -49,6 +58,7 @@ class GameView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
         when (event?.action) {
             MotionEvent.ACTION_DOWN -> {
+                movement.moveTo(x!!, y!!)
                 fingerPath.reset()
                 fingerPath.addCircle(x!!, y!!, 100.0f, Path.Direction.CW)
                 var region = Region()
@@ -71,11 +81,23 @@ class GameView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
             }
             MotionEvent.ACTION_UP -> {
+                if (!victory) {
+                    fingerPath.reset()
+                    movement.reset()
+                    currentCircle.reset()
+                    currentCircle.addCircle(5.0f, 5.9f, 50.0f, Path.Direction.CW)
+                    distance = 0f
+                }
+                invalidate()
 
             }
             MotionEvent.ACTION_MOVE -> {
                 fingerPath.reset()
                 fingerPath.addCircle(x!!, y!!, 100.0f, Path.Direction.CW)
+                movement.lineTo(x!!, y!!)
+                movementMeas.setPath(movement, false)
+                movement.reset()
+                movement.moveTo(x!!, y!!)
                 var region = Region()
                 val clip = Region(0, 0, width, height)
                 region.setPath(fingerPath, clip)
@@ -83,19 +105,19 @@ class GameView(context: Context, attrs: AttributeSet) : View(context, attrs) {
                 region2.setPath(currentCircle, clip)
                 if (region.op(region2, Region.Op.INTERSECT)) {
                     // Collision!
-                    Log.d("collision", "yes")
                     currentCircle.reset()
                     var pos: FloatArray = floatArrayOf(0f, 0f)
-                    distance += 10f
-                    Log.d("collision_distance", distance.toString())
+                    distance += movementMeas.length
                     if (distance > pathMeas.length) {
                         context.toast("VICTORY")
+                        victory = true
+                        distance = 0f
                         return true
                     }
                     pathMeas.getPosTan(distance, pos, null)
-                    Log.d("collisionpositions", pos[0].toString())
-                    currentCircle.addCircle(pos[0], pos[1], 10.0f, Path.Direction.CW)
+                    currentCircle.addCircle(pos[0], pos[1], 50.0f, Path.Direction.CW)
                 }
+                invalidate()
 
             }
         }
