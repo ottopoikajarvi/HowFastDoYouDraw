3e529c900cbb68220ece192f07136e64e2bc8a8d
juliushekkala <julius.hekkala@gmail.com>
2020-04-22 17:42:34 +0300

    Skip feature with acceleration sensor

--- app/src/main/java/com/mobComp2020/howfastdoyoudraw/PlayActivity.kt
+++ app/src/main/java/com/mobComp2020/howfastdoyoudraw/PlayActivity.kt
@@ -1,5 +1,10 @@
 package com.mobComp2020.howfastdoyoudraw
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
@@ -11,12 +16,25 @@ import kotlinx.android.synthetic.main.activity_play.*
 import kotlinx.android.synthetic.main.game_end.*
 import org.jetbrains.anko.doAsync
 import java.util.*
+import kotlin.math.pow
+import kotlin.math.sqrt
 
-class PlayActivity : AppCompatActivity() {
+class PlayActivity : AppCompatActivity(), SensorEventListener {
 
     private var isTimerRunning = false
     private var elapsedTime = 60
     private var points = 0
+    private lateinit var  sensorManager:SensorManager
+    private lateinit var linearAcceleration: Sensor
+
+    private var sensorEnabled = true
+
+    private var skipAmount = 3
+
+    private val skipThreshold = 30f
+    private val minTimeBetweenSkips = 1000
+    private var lastSkipTime: Long = 0
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,23 +49,41 @@ class PlayActivity : AppCompatActivity() {
         }
 
 
-        //Skip current figure
+        //Skip current figure if skips left
         skip_button.setOnClickListener {
-            this.gameView.skipCurrent()
+            var currentTime = System.currentTimeMillis()
+            if ((currentTime - lastSkipTime) > minTimeBetweenSkips) {
+                if (skipCheck()) {
+                    this.gameView.skipCurrent()
+                    lastSkipTime = currentTime
+                }
+            }
+
         }
 
         //Back button back to main menu
         back_from_play.setOnClickListener {
             finish()
         }
+
+        //Acceleration sensor
+        if (sensorEnabled) {
+
+            //Sensor stuff largely adapted from
+            // https://stackoverflow.com/questions/51710147/kotlin-using-motion-sensor
+            this.sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+            sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)?.let {
+                this.linearAcceleration = it
+            }
+        }
+
+
     }
 
     //This is called when the game ends
     private fun gameEnd() {
 
         points = GameView.getPointsEnd()
-
-        Log.d("pointAmount", points.toString())
         //Switch to game end view
         setContentView(R.layout.game_end)
         scoreText.setText(points.toString())
@@ -77,6 +113,24 @@ class PlayActivity : AppCompatActivity() {
 
         back_from_game_end.setOnClickListener {
             finish()
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        if (sensorEnabled) {
+            sensorManager.registerListener(
+                this,
+                this.linearAcceleration,
+                SensorManager.SENSOR_DELAY_GAME
+            )
+        }
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        if (sensorManager != null) {
+            sensorManager.unregisterListener(this)
         }
     }
 
@@ -124,7 +178,43 @@ class PlayActivity : AppCompatActivity() {
         return timerString
     }
 
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        //Unnecessary
+    }
 
+    override fun onSensorChanged(event: SensorEvent?) {
+        //Adapted from https://stackoverflow.com/a/32803134
+        if (event?.sensor?.type == Sensor.TYPE_LINEAR_ACCELERATION)  {
+            var currentTime = System.currentTimeMillis()
+            if ((currentTime - lastSkipTime) > minTimeBetweenSkips) {
+                var x = event.values[0]
+                var y = event.values[1]
+                var z = event.values[2]
+                var acceleration = sqrt(
+                    x.pow(2) + y.pow(2) + z.pow(2)
+                )
+                if (acceleration > skipThreshold) {
+                    lastSkipTime = currentTime;
+                    if (skipCheck()) {
+                        this.gameView.skipCurrent()
+                    }
+                }
+            }
+
+
+
+        }
+    }
+
+    //Check if the player has any skips left
+    private fun skipCheck(): Boolean {
+        if (skipAmount > 0) {
+            skipAmount -= 1
+            skip_text.text = "Skips left: " + skipAmount.toString()
+            return true
+        }
+        return false
+    }
 
 
 }
